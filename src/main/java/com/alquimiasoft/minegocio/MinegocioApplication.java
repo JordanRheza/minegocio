1216117adaf32cbe8c18831b677acb4431844fcc
Jordan <rhezajordan@gmail.com>
2025-04-18 15:26:08 -0500

    Add: Swagger and log4j dependency

--- src/main/java/com/alquimiasoft/minegocio/MinegocioApplication.java
+++ src/main/java/com/alquimiasoft/minegocio/MinegocioApplication.java
@@ -1,13 +1,16 @@
 package com.alquimiasoft.minegocio;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MinegocioApplication {
 
+	private static final Logger logger = LogManager.getLogger(MinegocioApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(MinegocioApplication.class, args);
+		logger.info("Se levantó el API");
 	}
-
 }
