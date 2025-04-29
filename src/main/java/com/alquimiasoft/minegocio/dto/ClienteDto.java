32411ad13680b3a45f35175c279c8ff580b5753b
Jordan <rhezajordan@gmail.com>
2025-04-29 00:51:12 -0500

    Modify: Cliente and Sucursal Dto

--- src/main/java/com/alquimiasoft/minegocio/dto/ClienteDto.java
+++ src/main/java/com/alquimiasoft/minegocio/dto/ClienteDto.java
@@ -1,9 +1,11 @@
-package com.alquimiasoft.minegocio.Dto;
+package com.alquimiasoft.minegocio.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 
 import java.util.List;
 
@@ -12,11 +14,25 @@ import java.util.List;
 @NoArgsConstructor
 public class ClienteDto {
 
+    @Schema(hidden = true)
     private Integer id;
+
+    @NotBlank(message = "no puede ser nulo, ní vacío")
+    @Size(max = 30, message = "no puede ser mayor a 30 caracteres")
     private String tipoIdentificacion;
+
+    @NotBlank(message = "no puede ser nulo, ní vacío")
+    @Size(max = 12, message = "no puede ser mayor a 12 caracteres")
     private String numeroIdentificacion;
+
+    @NotBlank(message = "no puede ser nulo, ní vacío")
+    @Size(max = 100, message = "no puede ser mayor a 100 caracteres")
     private String nombres;
+
+    @Size(max = 75, message = "no puede ser mayor a 75 caracteres")
     private String correo;
+
+    @Size(max = 10, message = "no puede ser mayor a 10 caracteres")
     private String telefono;
 
     private List<SucursalDto> sucursales;
--- src/main/java/com/alquimiasoft/minegocio/dto/SucursalDto.java
+++ src/main/java/com/alquimiasoft/minegocio/dto/SucursalDto.java
@@ -1,7 +1,10 @@
-package com.alquimiasoft.minegocio.Dto;
+package com.alquimiasoft.minegocio.dto;
 
-import lombok.AllArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -9,8 +12,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SucursalDto {
 
+    @Schema(hidden = true)
     private Integer id;
+
+    @NotBlank(message = "no puede ser nulo, ní vacío")
+    @Size(max = 100, message = "no puede ser mayor a 100 caracteres")
     private String provincia;
+
+    @NotBlank(message = "no puede ser nulo, ní vacío")
+    @Size(max = 100, message = "no puede ser mayor a 100 caracteres")
     private String ciudad;
+
+    @Size(max = 100, message = "no puede ser mayor a 100 caracteres")
     private String direccion;
+
+    @Schema(hidden = true)
+    private Boolean matriz;
 }
