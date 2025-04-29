e06a5ef5ab55d1aa89d390c8b966ea72d963ab62
Jordan <rhezajordan@gmail.com>
2025-04-29 00:57:51 -0500

    Add: Sucursal Repository

--- src/main/java/com/alquimiasoft/minegocio/repository/SucursalRepository.java
+++ src/main/java/com/alquimiasoft/minegocio/repository/SucursalRepository.java
@@ -1,7 +1,15 @@
-package com.alquimiasoft.minegocio.Repository;
+package com.alquimiasoft.minegocio.repository;
 
-import com.alquimiasoft.minegocio.Model.Sucursal;
+import com.alquimiasoft.minegocio.model.Sucursal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface SucursalRepository extends JpaRepository<Sucursal, Integer> {
+
+    List<Sucursal> findByClienteId(Integer clienteId);
+    boolean existsByClienteIdAndMatrizTrue(Integer clienteId);
+
 }
